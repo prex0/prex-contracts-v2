74f0d4f1c3d344643f65ab7e139818855dab5806
syuhei176 <syuhei176@gmail.com>
2025-03-20 21:55:35 +0900

    add PrexPoint

---
+++ src/PrexPoint.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
+import {IUserPoints} from "./interfaces/IUserPoints.sol";
+
+contract PrexPoint is IUserPoints, ERC20 {
+    constructor() ERC20("PrexPoint", "PREX", 18) {}
+    function consumePoints(
+        address user,
+        uint256 points
+    ) external {
+    }
+}
